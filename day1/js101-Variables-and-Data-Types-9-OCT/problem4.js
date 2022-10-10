5c5d4ea7ee862e158af5262595dd2540ca02594e
Juri Kalita <jurikalita011@gmail.com>
2022-10-10 18:03:17 +0530

    added day1 IW codes

---
+++ day1/js101-Variables-and-Data-Types-9-OCT/problem4.js
@@ -0,0 +1,17 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+let name="𝐑𝐚𝐦 𝐊𝐮𝐦𝐚𝐫";
+let school="𝙽𝚊𝚝𝚒𝚘𝚗𝚊𝚕 𝙿𝚞𝚋𝚕𝚒𝚌 𝚂𝚌𝚑𝚘𝚘𝚕";
+let grade="𝐕𝐈𝐈";
+let section="Ⓑ";
+let rollNumber="𝟯";
+let english=60;
+let science=80;
+let math=75;
+console.log("name:",name);
+console.log("school:",school);
+console.log("grade:",grade+" "+"section:",section+"  "+"roll number:",rollNumber);
+console.log("english:",english);
+console.log("science:",science);
+console.log("math:",math);
