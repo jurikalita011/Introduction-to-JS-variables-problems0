e6fe0e5edc6e42bc2dfe75223b8b72ff9f2bfa23
Juri Kalita <jurikalita011@gmail.com>
2022-10-11 21:06:54 +0530

    added day3 IW codes

---
+++ day3/js101-JavaScript-V-Logical-Operators-and-Ternary-OperatorIW/problem4.js
@@ -0,0 +1,8 @@
+// Problem 4: Given 3 numbers (all different values), print which is greatest
+
+let a=50;
+let b=10;
+let c=71;
+a>b&&a>c?console.log(a,"is greatest")
+  :b>a&&b>c?console.log(b,"is greatest")
+  :console.log(c,"is greatest");
