24fcc0ff9a1dcd103b3af0a57183e90cd4640d16
Juri Kalita <jurikalita011@gmail.com>
2022-10-12 13:17:44 +0530

    added day5 IW codes

---
+++ day5/js-101-JavaScript-VIII-Arrays-IW-12th-OCT/problem1.js
@@ -0,0 +1,6 @@
+// Problem 1 : Given an array print the position (starting with 1) and the element in a single line.
+
+let arr=[1,3,4,5,6,5,3];
+for(let i=0;i<=arr.length-1;i++){
+  console.log(i+1,arr[i]);
+}
